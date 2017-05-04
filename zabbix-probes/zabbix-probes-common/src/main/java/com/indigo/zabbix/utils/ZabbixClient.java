45f10b0e18d025ea9754b4ca2d1c6bcd2f7e57dc
jose-sanchezm <jose.sanchezm@atos.net>
2017-05-04 17:09:46 +0200

    Make code style plugin happy

--- zabbix-probes/zabbix-probes-common/src/main/java/com/indigo/zabbix/utils/ZabbixClient.java
+++ zabbix-probes/zabbix-probes-common/src/main/java/com/indigo/zabbix/utils/ZabbixClient.java
@@ -17,18 +17,18 @@ import java.util.stream.Collectors;
  * Created by jose on 22/09/16.
  */
 public class ZabbixClient {
-
+  
   public static final Integer ZABBIX_DEFAULT_PORT = 10051;
-
+  
   private String zabbixCategory;
   private String zabbixGroup;
   private String zabbixTemplate;
-
+  
   private static final Log logger = LogFactory.getLog(ZabbixClient.class);
-
+  
   private ZabbixWrapperClient wrapperClient;
   private ZabbixSender sender;
-
+  
   /**
    * Constructor used for testing.
    *
@@ -43,7 +43,7 @@ public class ZabbixClient {
     this.wrapperClient = wrapperClient;
     this.sender = sender;
   }
-
+  
   /**
    * Default constructor that will read the information from the configuration
    * properties.
@@ -54,24 +54,24 @@ public class ZabbixClient {
         new Integer(PropertiesManager.getProperty(
             ProbesTags.ZABBIX_PORT, ZABBIX_DEFAULT_PORT.toString())));
   }
-
+  
   /**
    * Default constructor.
    */
   public ZabbixClient(String wrapperEndpoint, String category, String group, String template,
-                      String zabbixHost,Integer zabbixPort) {
-
+                      String zabbixHost, Integer zabbixPort) {
+    
     wrapperClient = ProbeClientFactory.getZabbixWrapperClient(wrapperEndpoint);
-
+    
     Integer port = (zabbixPort != null) ? zabbixPort : ZABBIX_DEFAULT_PORT;
-
+    
     sender = new ZabbixSender(zabbixHost, port);
-
+    
     zabbixCategory = category;
     zabbixGroup = group;
     zabbixTemplate = template;
   }
-
+  
   /**
    * Ensure that a host is registered and if not, register it.
    *
@@ -79,7 +79,7 @@ public class ZabbixClient {
    * @return The registration status.
    */
   public boolean ensureRegistration(String host, boolean register) throws Exception {
-
+    
     Response hostInfo = wrapperClient.getHostInfo(host, zabbixGroup);
     if (hostInfo.status() < 300 && hostInfo.status() >= 200) {
       return true;
@@ -97,7 +97,7 @@ public class ZabbixClient {
       }
     }
   }
-
+  
   /**
    * Sends a series of metrics associated to a host.
    *
@@ -109,19 +109,19 @@ public class ZabbixClient {
         long timeSecs = metrics.getTimestamp() / 1000;
         String zabbixHost = PropertiesManager.getProperty(ProbesTags.ZABBIX_HOST);
         if (zabbixHost != null) {
-
+          
           List<DataObject> toSend = metrics.getMetrics().entrySet().stream().map(entry -> {
-
+            
             DataObject dataObject = new DataObject();
             dataObject.setHost(metrics.getHostName());
             dataObject.setKey(entry.getKey());
             dataObject.setValue(entry.getValue());
             dataObject.setClock(timeSecs);
-
+            
             return dataObject;
           })
-              .collect(Collectors.toList());
-
+                                        .collect(Collectors.toList());
+          
           try {
             SenderResult sendResult = sender.send(toSend);
             if (!sendResult.success()) {
@@ -134,19 +134,19 @@ public class ZabbixClient {
               sendResult = sender.send(toSend);
               if (!sendResult.success()) {
                 logger.error("Error sending values for host " + metrics.getHostName() + ": "
-                    + "\nTotal: " + sendResult.getTotal() + "\nProcessed: "
-                    + sendResult.getProcessed() + "\nFailed: " + sendResult.getFailed() + ": "
-                    + metrics.getMetrics().toString());
+                                 + "\nTotal: " + sendResult.getTotal() + "\nProcessed: "
+                                 + sendResult.getProcessed() + "\nFailed: "
+                                 + sendResult.getFailed() + ": " + metrics.getMetrics().toString());
               } else {
                 logger.info("Successfully sent " + sendResult.getProcessed()
-                    + " metrics for host " + metrics.getHostName());
+                                + " metrics for host " + metrics.getHostName());
               }
             } else {
               logger.info("Successfully sent " + sendResult.getProcessed()
-                  + " metrics for host " + metrics.getHostName());
+                              + " metrics for host " + metrics.getHostName());
             }
             return sendResult;
-
+            
           } catch (IOException e) {
             logger.error("Error sending values for host " + metrics.getHostName(), e);
           }
@@ -157,5 +157,5 @@ public class ZabbixClient {
     }
     return null;
   }
-
+  
 }
