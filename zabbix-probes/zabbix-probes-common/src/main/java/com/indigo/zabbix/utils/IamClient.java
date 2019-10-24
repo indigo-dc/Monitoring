fecb4d81c01f674ed71adf2d447d2149d300d553
Jose Antonio Sanchez <jose.sanchezm@atos.net>
2019-10-24 10:54:31 +0200

    Fix code style for IAM client

--- zabbix-probes/zabbix-probes-common/src/main/java/com/indigo/zabbix/utils/IamClient.java
+++ zabbix-probes/zabbix-probes-common/src/main/java/com/indigo/zabbix/utils/IamClient.java
@@ -26,116 +26,120 @@ import java.net.URISyntaxException;
 /** Created by jose on 31/03/17. */
 public class IamClient {
 
-	private static final Log logger = LogFactory.getLog(IamClient.class);
+  private static final Log logger = LogFactory.getLog(IamClient.class);
 
-	private static OIDCTokens getAccessToken(String location, AuthorizationGrant grant, String clientId,
-			String clientSecret) {
-		try {
-			// The credentials to authenticate the client at the token endpoint
-			ClientID clientIdObj = new ClientID(clientId);
-			Secret clientSecretObj = new Secret(clientSecret);
-			ClientAuthentication clientAuth = new ClientSecretBasic(clientIdObj, clientSecretObj);
+  private static OIDCTokens getAccessToken(
+      String location, AuthorizationGrant grant, String clientId, String clientSecret) {
+    try {
+      // The credentials to authenticate the client at the token endpoint
+      ClientID clientIdObj = new ClientID(clientId);
+      Secret clientSecretObj = new Secret(clientSecret);
+      ClientAuthentication clientAuth = new ClientSecretBasic(clientIdObj, clientSecretObj);
 
-			// The token endpoint
-			URI tokenEndpoint = new URI(location);
+      // The token endpoint
+      URI tokenEndpoint = new URI(location);
 
-			// Make the token request
-			TokenRequest request = new TokenRequest(tokenEndpoint, clientAuth, grant);
+      // Make the token request
+      TokenRequest request = new TokenRequest(tokenEndpoint, clientAuth, grant);
 
-			TokenResponse tokenResponse = OIDCTokenResponseParser.parse(request.toHTTPRequest().send());
+      TokenResponse tokenResponse = OIDCTokenResponseParser.parse(request.toHTTPRequest().send());
 
-			if (!tokenResponse.indicatesSuccess()) {
-				// We got an error response...
-				TokenErrorResponse errorResponse = tokenResponse.toErrorResponse();
-				logger.error("Error getting access token: " + errorResponse.toJSONObject().toJSONString());
-			} else {
-				OIDCTokenResponse successResponse = (OIDCTokenResponse) tokenResponse.toSuccessResponse();
-				return successResponse.getOIDCTokens();
-			}
+      if (!tokenResponse.indicatesSuccess()) {
+        // We got an error response...
+        TokenErrorResponse errorResponse = tokenResponse.toErrorResponse();
+        logger.error("Error getting access token: " + errorResponse.toJSONObject().toJSONString());
+      } else {
+        OIDCTokenResponse successResponse = (OIDCTokenResponse) tokenResponse.toSuccessResponse();
+        return successResponse.getOIDCTokens();
+      }
 
-		} catch (ParseException e) {
-			logger.error("Error parsing access token", e);
-		} catch (IOException e) {
-			logger.error("Error sending token request", e);
-		} catch (URISyntaxException e) {
-			logger.error("Invalid location URL", e);
-		}
+    } catch (ParseException e) {
+      logger.error("Error parsing access token", e);
+    } catch (IOException e) {
+      logger.error("Error sending token request", e);
+    } catch (URISyntaxException e) {
+      logger.error("Invalid location URL", e);
+    }
 
-		return null;
-	}
+    return null;
+  }
 
-	/**
-	 * Gets an access token from the IAM service.
-	 *
-	 * @param location     Location of the IAM server.
-	 * @param username     Username for the authorized user.
-	 * @param password     Password for the authorized user.
-	 * @param clientId     Client ID to authorize.
-	 * @param clientSecret Client secret of the client ID application.
-	 * @return An access token for the provided user and application.
-	 */
-	public static OIDCTokens getAccessToken(String location, String username, String password, String clientId,
-			String clientSecret) {
-		AuthorizationGrant grant;
-		String grantType;
-		try {
-			grantType = PropertiesManager.getProperty(ProbesTags.IAM_GRANT_TYPE);
-			if (grantType.equals(ProbesTags.GRANT_TYPE_CLIENT_CREDENTIALS))
-				grant = new ClientCredentialsGrant();
-			else
-				grant = new ResourceOwnerPasswordCredentialsGrant(username, new Secret(password));
-		} catch (NullPointerException e) {
-			grant = new ResourceOwnerPasswordCredentialsGrant(username, new Secret(password));
-		}
-		
-		return getAccessToken(location, grant, clientId, clientSecret);
-	}
+  /**
+   * Gets an access token from the IAM service.
+   *
+   * @param location Location of the IAM server.
+   * @param username Username for the authorized user.
+   * @param password Password for the authorized user.
+   * @param clientId Client ID to authorize.
+   * @param clientSecret Client secret of the client ID application.
+   * @return An access token for the provided user and application.
+   */
+  public static OIDCTokens getAccessToken(
+      String location, String username, String password, String clientId, String clientSecret) {
+    AuthorizationGrant grant;
+    String grantType;
+    try {
+      grantType = PropertiesManager.getProperty(ProbesTags.IAM_GRANT_TYPE);
+      if (grantType.equals(ProbesTags.GRANT_TYPE_CLIENT_CREDENTIALS)) {
+        grant = new ClientCredentialsGrant();
+      } else {
+        grant = new ResourceOwnerPasswordCredentialsGrant(username, new Secret(password));
+      }
+    } catch (NullPointerException e) {
+      grant = new ResourceOwnerPasswordCredentialsGrant(username, new Secret(password));
+    }
 
-	/**
-	 * Utility method that will return an access token from parameters provided in
-	 * the configuration file. - iam.location: Location of the IAM server. -
-	 * iam.username: Username for the authorized user. - iam.password: Password for
-	 * the authorized user. - iam.clientid: Client ID to authorize. -
-	 * iam.clientsecret: Client secret of the client ID application.
-	 *
-	 * @return An access token for the provided user and application.
-	 */
-	public static OIDCTokens getAccessToken() {
+    return getAccessToken(location, grant, clientId, clientSecret);
+  }
 
-		return getAccessToken(PropertiesManager.getProperty(ProbesTags.IAM_LOCATION),
-				PropertiesManager.getProperty(ProbesTags.IAM_USERNAME),
-				PropertiesManager.getProperty(ProbesTags.IAM_PASSWORD),
-				PropertiesManager.getProperty(ProbesTags.IAM_CLIENTID),
-				PropertiesManager.getProperty(ProbesTags.IAM_CLIENTSECRET));
-	}
+  /**
+   * Utility method that will return an access token from parameters provided in the configuration
+   * file. - iam.location: Location of the IAM server. - iam.username: Username for the authorized
+   * user. - iam.password: Password for the authorized user. - iam.clientid: Client ID to authorize.
+   * - iam.clientsecret: Client secret of the client ID application.
+   *
+   * @return An access token for the provided user and application.
+   */
+  public static OIDCTokens getAccessToken() {
 
-	/**
-	 * Refresh a potentially expired token.
-	 *
-	 * @param iamLocation       Location of the IAM instance.
-	 * @param refreshTokenValue Refresh token value.
-	 * @param clientId          Client ID to use for refresh request.
-	 * @param clientSecretValue Client secret to use for the refresh request.
-	 * @return A new access and refresh token.
-	 */
-	public static OIDCTokens refreshToken(String iamLocation, String refreshTokenValue, String clientId,
-			String clientSecretValue) {
+    return getAccessToken(
+        PropertiesManager.getProperty(ProbesTags.IAM_LOCATION),
+        PropertiesManager.getProperty(ProbesTags.IAM_USERNAME),
+        PropertiesManager.getProperty(ProbesTags.IAM_PASSWORD),
+        PropertiesManager.getProperty(ProbesTags.IAM_CLIENTID),
+        PropertiesManager.getProperty(ProbesTags.IAM_CLIENTSECRET));
+  }
 
-		return getAccessToken(iamLocation, new RefreshTokenGrant(new RefreshToken(refreshTokenValue)), clientId,
-				clientSecretValue);
-	}
+  /**
+   * Refresh a potentially expired token.
+   *
+   * @param iamLocation Location of the IAM instance.
+   * @param refreshTokenValue Refresh token value.
+   * @param clientId Client ID to use for refresh request.
+   * @param clientSecretValue Client secret to use for the refresh request.
+   * @return A new access and refresh token.
+   */
+  public static OIDCTokens refreshToken(
+      String iamLocation, String refreshTokenValue, String clientId, String clientSecretValue) {
 
-	/**
-	 * Gets new access and refresh tokens by getting the IAM information from the
-	 * properties file.
-	 *
-	 * @param refreshToken The refresh token to use.
-	 * @return A new access and refresh token.
-	 */
-	public static OIDCTokens refreshToken(String refreshToken) {
-		return refreshToken(PropertiesManager.getProperty(ProbesTags.IAM_LOCATION), refreshToken,
-				PropertiesManager.getProperty(ProbesTags.IAM_CLIENTID),
-				PropertiesManager.getProperty(ProbesTags.IAM_CLIENTSECRET));
-	}
+    return getAccessToken(
+        iamLocation,
+        new RefreshTokenGrant(new RefreshToken(refreshTokenValue)),
+        clientId,
+        clientSecretValue);
+  }
+
+  /**
+   * Gets new access and refresh tokens by getting the IAM information from the properties file.
+   *
+   * @param refreshToken The refresh token to use.
+   * @return A new access and refresh token.
+   */
+  public static OIDCTokens refreshToken(String refreshToken) {
+    return refreshToken(
+        PropertiesManager.getProperty(ProbesTags.IAM_LOCATION),
+        refreshToken,
+        PropertiesManager.getProperty(ProbesTags.IAM_CLIENTID),
+        PropertiesManager.getProperty(ProbesTags.IAM_CLIENTSECRET));
+  }
 }
-
