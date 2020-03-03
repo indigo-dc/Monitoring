5bce06b3760f0980308f5788ade6dfcec3c6ecf8
Rut Palmero <ruth.palmero@atos.net>
2020-03-03 14:31:49 +0100

    Adding bash script and log properties

---
+++ zabbix-probes/openstack-zabbix-probe/openstackprobe.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+COMMAND=$1
+LOG_CONF="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Jdk14Logger -Djava.util.logging.config.file=/etc/zabbix/openstackprobe-log.properties"
+BASE="java $LOG_CONF -cp /usr/share/java/zabbix/openstack-zabbix-probe.jar"
+
+$BASE org.indigo.openstackprobe.openstack.QCGThread ${@:2}
